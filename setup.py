96a0975e968de617f965da63050af03d524105b6
vikasghandge <ghandgevikas804@gmail.com>
2026-05-24 15:42:46 +0530

    added requirement.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup( 
+    name='my_package',
+    version='0.1.0',
+    author='Vikas ghandge',
+    author_email='vikas.ghandge@example.com',
+    packages=find_packages(),
+    install_requires=[]
+)
